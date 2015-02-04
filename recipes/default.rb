f7230cd87563ade2861403bc6ffd429c80012443
Joris van Lieshout <jvanlieshout@schubergphilis.com>
2015-02-04 17:09:22 +0100

    update documentation

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,6 @@
 # Cookbook Name:: sbp_mremoteng
 # Recipe:: default
 #
-# Copyright 2014, Schubergp Philis
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
